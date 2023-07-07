7874cd8a2b20d7b803288c3fb00554f6b51074c6
LuaSavage <ilya_generic_post@yahoo.com>
2023-07-08 00:44:49 +0300

    implemented gracefull shutdown

--- service-a/app/cmd/main.go
+++ service-a/app/cmd/main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
-	haccount "github.com/LuaSavage/bwg-test-task/service-a/internal/adapter/api/account"
+	acchandler "github.com/LuaSavage/bwg-test-task/service-a/internal/adapter/api/account"
 	"github.com/LuaSavage/bwg-test-task/service-a/internal/adapter/msgbroker"
 	"github.com/LuaSavage/bwg-test-task/service-a/internal/config"
 	"github.com/LuaSavage/bwg-test-task/service-a/internal/domain/account"
-	"github.com/LuaSavage/bwg-test-task/service-a/pkg/msgbroker/dto"
+	kafkaclient "github.com/LuaSavage/bwg-test-task/service-a/pkg/msgbroker"
 	"github.com/labstack/echo/v4"
 )
 
@@ -19,18 +25,33 @@ func main() {
 	}
 
 	// initialising Kafka service
-	producerCfg := &dto.NewProducerDTO{
-		BrokerAdress: fmt.Sprintf("%s:%s", cfg.Kafka.Host, cfg.Kafka.Port),
-		Topic:        cfg.Kafka.Topic,
-		Logger:       e.Logger,
-	}
-	msgBroker, err := msgbroker.NewKafkaService(producerCfg)
+	kafkaProducer, err := kafkaclient.NewKafkaProducer(cfg.Kafka, e.Logger)
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
+	defer kafkaProducer.Close()
+	msgBroker := msgbroker.NewKafkaService(kafkaProducer, e.Logger)
+
+	accountService := account.NewService(msgBroker)
 
 	// initialising http handler's
-	accountService := account.NewService(msgBroker)
-	accountHandler := haccount.NewHandler(accountService /*rate limit*/, cfg.Listen.RPC)
+	accountHandler := acchandler.NewHandler(accountService /*rate limit*/, cfg.Listen.RPC)
 	accountHandler.Register(e)
+
+	err = e.Start(fmt.Sprintf("%s:%s", cfg.Listen.BindIP, cfg.Listen.Port))
+	if err != nil && err != http.ErrServerClosed {
+		e.Logger.Fatal("shutting down the server")
+	}
+
+	osSignal := make(chan os.Signal)
+	signal.Notify(osSignal, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	receivedSignal := <-osSignal
+
+	e.Logger.Infof("Application exit. Signal: %s (%d)", receivedSignal.String(), receivedSignal)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
